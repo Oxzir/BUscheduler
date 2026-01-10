0866be60c53188eb4d266698afcf674c9fd5cc64
Oxzir <41166510+Oxzir@users.noreply.github.com>
2026-01-10 09:43:23 +0100

    Enhance script to wait until target time and log
    Refactor script to wait until a specific time and log execution.

--- script.py
+++ script.py
@@ -1,8 +1,24 @@
-from datetime import datetime
+from datetime import datetime, time
+from zoneinfo import ZoneInfo
+import time as sleep_time
 
-now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
+timezone = ZoneInfo("Europe/Paris")
+target_time = time(10,0)
+launchtime = datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S %Z")
 
+def wait_until_midnight():
+    while True:
+        now = datetime.now(timezone)
+        if now.time() >= target_time:
+            return now.strftime('%Y-%m-%d %H:%M:%S %Z')
+        print(f"{now.strftime('%H:%M:%S')} : en attente de {target_time}")
+        sleep_time.sleep(5)
+
+wait_until_midnight()
 with open("last_run.txt", "w", encoding="utf-8") as f:
-    f.write(f"Dernière exécution : {now}\n")
+    f.write(
+        f"Lancement à {launchtime}\n"
+        f"Exécution à {wait_until_midnight()}\n"
+    )
 
 print("Heure :", now)
